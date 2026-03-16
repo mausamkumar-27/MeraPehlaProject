3e821ae7da7fdfeab68546f71daf2d6925d30c86
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-03-16 12:47:19 +0000

    "C tutorial 1 code"

--- main.cpp
+++ main.cpp
@@ -1,7 +1,1 @@
-#include <iostream> 
-using namespace std;
 
-int main() {
-    cout << "mubarak ho mausam ! ipad par c++ chal gayi." << endl;
-    return 0;
-}
---
+++ tutorial1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("hello mausam kaise ho.");
+    return 0;
+
+}
