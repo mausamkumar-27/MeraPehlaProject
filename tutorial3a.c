9c09c45984c1c1014dbae1e3fb2f02f05199b136
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-03-18 17:53:09 +0000

    tutorial3a: table of 19

---
+++ tutorial3a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a=19;
+    printf("19*1=%d\n", a*1);
+    printf("19*2=%d\n", a*2);
+    printf("19*3=%d\n", a*3);
+    printf("19*4=%d\n", a*4);
+    printf("19*5=%d\n", a*5);
+    printf("19*6=%d\n", a*6);
+    printf("19*7=%d\n", a*7);
+    printf("19*8=%d\n", a*8);
+    printf("19*9=%d\n", a*9);
+    printf("19*10=%d\n", a*10);
+    return 0;
+}
