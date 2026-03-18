0a4ef4736f6304e538d696a73579b87c293de2d0
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-03-18 06:21:10 +0000

    tutorial:2a multiply wala program part2

--- tutorial2a.c
+++ tutorial2a.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
+int main(){ int a,b ;
 
-int main() {
-    int a, b;
-    printf("Enter number a: ");
-    scanf("%d", &a);
-
-    printf("Enter number b: ");
-    scanf("%d", &b);
-    printf("the product is %d", a*b);
-
+    printf("Enter number a");
+    scanf("%d", & a);
+    printf("Enter number b");
+    scanf("%d", & b);
+    printf("The product of %d \n", a*b);
     return 0;
 }
