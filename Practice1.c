422a01374012b030e785a38cf6544eef3763a18d
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-03-19 06:10:44 +0000

    Prctice 1

---
+++ Practice1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int c=25;
+    printf("25*1=%d\n", c*1);
+    printf("25*2=%d\n", c*2);
+    printf("25*3=%d\n", c*3);
+    printf("25*4=%d\n", c*4);
+    printf("25*5=%d\n", c*5);
+    printf("25*6=%d\n", c*6);
+    printf("25*7=%d\n", c*7);
+    printf("25*8=%d\n", c*8);
+    printf("25*9=%d\n", c*9);
+    printf("25*10=%d\n", c*10);
+    return 0;
+}
