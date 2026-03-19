8a853971dee1cca8914bcb3b88bebd29607ae923
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-03-19 06:27:10 +0000

    Practice 2

---
+++ Practice2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int num1=13;
+    int num2=17;
+    printf("num1+num2=%d\n", num1 + num2);
+    printf("num1-num2=%d\n", num1 - num2);
+    printf("num1*num2=%d\n", num1 * num2);
+    printf("num1/num2=%d\n", num1 / num2);
+    printf("num1%%num2=%d\n", num1 % num2);
+    return 0;
+
+}
