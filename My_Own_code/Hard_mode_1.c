b1b40ebcbc9beb1a8507e58903fcc1652fda207c
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-03-19 16:35:49 +0000

    Hard mode 1

---
+++ My_Own_code/Hard_mode_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a=25;
+    printf("25*10=%d\n", a*10);
+    printf("25*9=%d\n", a*9);
+    printf("25*8=%d\n", a*8);
+    printf("25*7=%d\n", a*7);
+    printf("25*6=%d\n", a*6);
+    printf("25*5=%d\n", a*5);
+    printf("25*4=%d\n", a*4);
+    printf("25*3=%d\n", a*3);
+    printf("25*2=%d\n", a*2);
+    printf("25*1=%d\n", a*1);
+    return 0;
+
+}
