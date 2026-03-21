4600bb333c4f173a85c8a5f72d010c0edd245cc8
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-03-21 13:38:38 +0000

    tutorial15a : for loop

---
+++ .vscode/tutorial15a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int balls;
+    printf("over shuru ho raha hai");
+    for (int i=1; i<=6; i++) {
+        printf("ball number %d phenki gyi\n", i);
+    }
+    printf("over khatam ho gaya hai");
+    return 0;
+}
