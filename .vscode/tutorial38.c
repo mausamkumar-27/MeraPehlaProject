4a26d31350bff22b0625e1dc92e72816c745e173
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-04-10 15:09:52 +0000

    tutorial38: Basic typedef use

---
+++ .vscode/tutorial38.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+    typedef unsigned long ul;
+    typedef int integer;
+    //ul ,l1,l2,l3;   datatype aur pehle variable ke beech keval space hota hai comma nahi
+    ul l1,l2,l3;
+    integer a=4;
+    printf("value of a is %d",a);
+    return 0;
+}
