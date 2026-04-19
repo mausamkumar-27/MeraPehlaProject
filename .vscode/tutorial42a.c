920ac07347eb95b1032757e82ca25dd99aca624d
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-04-19 08:04:54 +0000

    tutorial42a:local& global variable basic

--- .vscode/tutorial42a.c
+++ .vscode/tutorial42a.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int b=34;
-//int func1(intb)   tumne int aur b ke beech space nahi diya islye o\p 34,340 diya
+//int func1(intb)   tumne int aur b ke beech space nahi diya islye o\p me 34,340 diya
 int func1(int b)
 {
     printf("the value of b inside func1 is %d\n",b);
-    return b * 10;
+    return b* 10;
 }
-int main() 
+int main()
 {
     int b=344;
     int val=func1(b);
     int *ptr =&val;
-    printf("the value of finc1 is %d\n",val);
+    printf("the value of b inside func1 is %d\n",val);
     return 0;
 }
