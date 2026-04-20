10171429fe0050da11aa5e8dd946d9edb98042a9
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-04-20 06:57:39 +0000

    tutorial47:again use of malloc

--- .vscode/tutorial47.c
+++ .vscode/tutorial47.c
@@ -5,9 +5,10 @@ int main() {
     ptr = (int *)malloc(3*sizeof(int));
     for(int i=0; i<3; i++) {
         printf("Enter the value of an array:\n", i);
-        scanf("%d", &ptr[i]);
+       // scanf("%d", &ptr[i]);  ab initialised to garbage value hogi chahe 0ho ya koi random value
+       scanf("%d", &ptr[i]);
     }
-    for(int i=0; i<3; i++) {   // i<5 kroge to garbage value bhi print hogi
+    for(int i=0; i<3; i++) {  
         printf("The value of an array is: %d\n", ptr[i]);
     }
     return 0;
