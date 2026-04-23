58eea45f5d6678e909bceaf27e969a9a9f3c04db
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-04-23 10:02:01 +0000

    tutorial49b: use of Static variable

---
+++ .vscode/tutorial49b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void counter() {
+    static int count = 0;
+    count++;
+    printf("Counter ki value hai: %d\n", count);
+}
+int main() {
+    printf("---Pehli baar call---\n");
+    counter();
+    printf("---Dusri baar call---\n");
+    counter();
+    printf("---Teesri baar call---\n");
+    counter();
+    return 0;
+}
