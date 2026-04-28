6f83a5efac05143382311dd7a3bea126fffd15bf
mausamkumar-27 <mausamkumar2717@gmail.com>
2026-04-28 11:47:32 +0000

    tutorial60: use of predefined macros

---
+++ .vscode/tutorial60.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("FILE NAME is %s\n", __FILE__);
+   // printf("Now TIME is %d\n", __TIME__);
+   printf(" Now TIME is %s\n", __TIME__);
+    printf("Today's DATE is %s\n", __DATE__);
+    printf("LINE number is %d\n", __LINE__);    // do line kheenchna (__)
+    return 0;
+
+
+}
